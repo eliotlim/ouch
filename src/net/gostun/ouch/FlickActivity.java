aa65b8dc47ae973cb90993bc1b1ad7b308c29153
eliot.lim <eliot.lim@gmail.com>
2015-09-27 12:45:56 +0800

    + Added ability to arrange notes on screen via Move actions + Added a 'fanned' view of cash on the FlickActivity

--- src/net/gostun/ouch/FlickActivity.java
+++ src/net/gostun/ouch/FlickActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.FrameLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 import net.gostun.ouch.moneypack.AbstractCash;
 import net.gostun.ouch.moneypack.AbstractMoneyPack;
@@ -30,8 +31,8 @@ public class FlickActivity extends Activity {
 		 * Retrieve and validate entered amount
 		 * from the intent.
 		 */
-		Intent i = getIntent();
-		float amount = i.getFloatExtra("amount", Float.NaN);
+		Intent intent = getIntent();
+		float amount = intent.getFloatExtra("amount", Float.NaN);
 		if (amount == Float.NaN) {
 			Toast.makeText(this, R.string.warning_invalid_amount, Toast.LENGTH_SHORT).show();
 		}
@@ -41,11 +42,20 @@ public class FlickActivity extends Activity {
 		 * change-making algorithm to MoneyPack.
 		 */
 		try {
-			AbstractMoneyPack moneyPack = (AbstractMoneyPack) Class.forName(i.getStringExtra("MoneyPack")).newInstance();
+			AbstractMoneyPack moneyPack = (AbstractMoneyPack) Class.forName(intent.getStringExtra("MoneyPack")).newInstance();
 			AbstractCash[] cashList = moneyPack.getCash(amount, this);
-			for (AbstractCash cash : cashList) {
-				if (cash != null)
-					flickView.addView(cash);
+			if (cashList.length <= 0) {
+				TextView tv = (TextView) findViewById(R.id.statusText);
+				tv.setText(R.string.warning_invalid_amount);
+			} else {
+				int fanStartD = 60, fanEndD = 120, i = 0;
+				float inc = (cashList.length == 1) ? 0 : (fanEndD - fanStartD) / (float) cashList.length - 1;
+				for (AbstractCash cash : cashList) {
+					if (cash != null) {
+						flickView.addView(cash);
+						cash.setRotation(fanStartD + inc * (i++));
+					}
+				}
 			}
 		} catch (InstantiationException | IllegalAccessException | ClassNotFoundException e) {
 			e.printStackTrace();
--- src/net/gostun/ouch/moneypack/AbstractCash.java
+++ src/net/gostun/ouch/moneypack/AbstractCash.java
@@ -2,6 +2,8 @@ package net.gostun.ouch.moneypack;
 
 import android.content.Context;
 import android.graphics.drawable.Drawable;
+import android.view.MotionEvent;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 
 public abstract class AbstractCash extends ImageView {
@@ -9,5 +11,49 @@ public abstract class AbstractCash extends ImageView {
 	public AbstractCash(Context context, Drawable drawable) {
 		super(context);
 		this.setImageDrawable(drawable);
+
+		this.setClickable(true);
+		setLayoutParams(new FrameLayout.LayoutParams(FrameLayout.LayoutParams.WRAP_CONTENT, FrameLayout.LayoutParams.WRAP_CONTENT));
+		setAdjustViewBounds(true);
+
+		setPivotX(0);
+		setPivotY(0);
+		setTranslationX(200);
 	}
+
+/*	public void setPosition(float x, float y) {
+		setTranslationX(x);
+		setTranslationY(y);
+	}*/
+
+	private float beginTX, beginTY, beginX, beginY;
+
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		int action = event.getActionMasked();
+
+		switch (action) {
+			case (MotionEvent.ACTION_DOWN):
+				beginX = event.getRawX();
+				beginY = event.getRawY();
+				beginTX = getTranslationX();
+				beginTY = getTranslationY();
+				return true;
+			case (MotionEvent.ACTION_MOVE):
+				setTranslationX(event.getRawX() - beginX + beginTX);
+				setTranslationY(event.getRawY() - beginY + beginTY);
+				return true;
+			case (MotionEvent.ACTION_UP):
+				//Log.i("ouch", "Translation - Original: ["+beginTX+", "+beginTY+"] New: ["+getTranslationX()+", "+getTranslationY()+"]");
+				//Log.i("ouch", "TouchEvents - Original: ["+beginX+", "+beginY+"] New: ["+event.getRawX()+", "+event.getRawY()+"]");
+				return true;
+			case (MotionEvent.ACTION_CANCEL):
+				return true;
+			case (MotionEvent.ACTION_OUTSIDE):
+				return true;
+			default:
+				return super.onTouchEvent(event);
+		}
+	}
+
 }
